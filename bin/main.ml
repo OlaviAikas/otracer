10d82a858b5b9bb3d33d227614261ecbebdda086
OlaviAikas <olavi.aikas@protonmail.com>
2023-03-23 17:51:13 +0200

    Switch to imagelib for portability

--- bin/main.ml
+++ bin/main.ml
@@ -1,11 +1,4 @@
-open Images
 open Raytracing
-
-let rgb_of_vect (v: Vect.vect) =
-  let fr, fg, fb = v in
-  {r = int_of_float fr;
-   g = int_of_float fg;
-   b = int_of_float fb;}
 
 let clamp_colour (v: Vect.vect) : Vect.vect =
   let fr, fg, fb = v in
@@ -18,7 +11,7 @@ let gamma_correct (gamma: float) (v: Vect.vect) : Vect.vect =
   (Float.pow fr gamma, Float.pow fg gamma, Float.pow fb gamma)
   
 let main () =
-  let img = Rgb24.create Params.width Params.height in
+  let img = Image.create_rgb Params.width Params.height in
   let scene = Scene_loader.load_scene "scene.toml" in
   let camera = Camera.new_cam (0.0, 2.0, 0.0) (0.0, 0.0, 1.0) (0.0, 1.0, 0.0) 1.04 in
   let () = print_string "Scene loaded. Rendering...\n" in
@@ -27,12 +20,13 @@ let main () =
       for j = 0 to (Params.width - 1) do
         let ray = Camera.ray camera i j in
         let c = Scene.get_colour ray scene Params.rendering_depth in
-        let im_colour = c |> (gamma_correct 0.45) |> clamp_colour |> rgb_of_vect in
-        Rgb24.set img j i im_colour
+        let im_colour = c |> (gamma_correct 0.45) |> clamp_colour in
+        let (r, g, b) = im_colour in
+        Image.write_rgb img j i (int_of_float r) (int_of_float g) (int_of_float b)
       done
     done in
     let () = print_string "Rendering completed. Saving...\n" in
-    let () = Png.save Params.filename [] (Rgb24(img)) in
+    let () = ImageLib_unix.writefile Params.filename img in
     print_string ("Image saved to '" ^ Params.filename ^ "'\n")
 
 let () = main ()
